c7ee6f7233008e7de271d17c1deb8bf6339438c3
Jakob Jul Elben <elbenjakobjul@gmail.com>
2018-09-06 14:25:32 +0200

    Add tests and tweak CI's

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,54 +1,17 @@
-# The testfile is inspired by pypackage
-# See https://github.com/audreyr/cookiecutter-pypackage
-
-from cookiecutter.main import cookiecutter
-from shutil import rmtree
-
+from cookiecutter import main
 import os
-import shlex
-import subprocess
-
-
-class within_dir(object):
-    def __init__(self, path):
-        self.new_path = path
-        self.old_path = os.getcwd()
-
-    def __enter__(self):
-        os.chdir(self.new_path)
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        os.chdir(self.old_path)
+import shutil
 
 
 class TempCookiecutterProject(object):
-    def __init__(self, template=None):
-        if template:
-            self.template = os.path.abspath(template)
-        else:
-            self.template = os.path.abspath(".")
-
     def __enter__(self):
-        cookiecutter(self.template, no_input=True)
-        return self
+        main.cookiecutter('.', no_input=True)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        rmtree("pyawesome")
+        shutil.rmtree(os.path.abspath('./pyawesome'))
 
 
-def check_call_within_dir(cmd, path):
-    with within_dir(path):
-        return subprocess.check_call(shlex.split(cmd))
-
-
-def check_output_within_dir(cmd, path):
-    with within_dir(path):
-        return subprocess.check_output(shlex.split(cmd))
-
-
-def test_temp_project_works():
+def test_project():
     with TempCookiecutterProject():
-        assert os.path.isdir('pyawesome')
-        assert os.path.isdir('pyawesome/pyawesome')
-        assert os.path.isfile('pyawesome/pyawesome/__init__.py')
+        assert os.path.isdir(os.path.abspath('./pyawesome'))
 
